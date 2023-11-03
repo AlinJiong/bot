2ebd684fd865883cfac129722187f9a8e2b783c1
AlinJiong <alinjiong@qq.com>
2023-11-03 22:14:09 +0800

    修改微博热搜api接口

--- plugins/bot_auto_hotlist.py
+++ plugins/bot_auto_hotlist.py
@@ -54,28 +54,27 @@ async def long_to_short(origin_url: str):
 
 async def get_HotList(choice: str = "weibo"):
     "获取微博热搜"
-    async with httpx.AsyncClient() as client:
-        try:
-            response = requests.get(url="https://tenapi.cn/resou/", timeout=10)
-            text_to_dic = json.loads(response.text)
-            data = text_to_dic["list"]
-            content = "#实时微博热搜#\n"
-            for i in range(0, 10):
-                link = await long_to_short(data[i]["url"])
-                content += str(i) + "." + data[i]["name"] + "\n" + link + "\n"
-                time.sleep(random.randint(5, 8))
+    try:
+        response = requests.get(url="http://api.xtaoa.com/api/weibo.php", timeout=10)
+        text_to_dic = json.loads(response.text)
+        data = text_to_dic["data"]
+        content = "#实时微博热搜#\n"
+        for i in range(0, 10):
+            link = await long_to_short(data[i]["url"])
+            content += str(i) + "." + data[i]["title"] + "\n" + link + "\n"
+            time.sleep(random.randint(5, 8))
 
-            action = Action(qq=jconfig.bot)
-            await action.sendGroupText(773933325, content)
-            time.sleep(5)
-            await action.sendFriendText(jconfig.superAdmin, content)
-            # await action.close()
-            logger.info("发送微博热搜！")
+        action = Action(qq=jconfig.bot)
+        await action.sendGroupText(773933325, content)
+        time.sleep(5)
+        await action.sendFriendText(jconfig.superAdmin, content)
+        # await action.close()
+        logger.info("发送微博热搜！")
 
-        except Exception as e:
-            logger.info(e)
-            logger.info("自动获取微博热搜失败！")
-            return None
+    except Exception as e:
+        logger.info(e)
+        logger.info("自动获取微博热搜失败！")
+        return None
 
 
 job1 = async_scheduler.add_job(get_HotList, "cron", hour=9, minute=5)
